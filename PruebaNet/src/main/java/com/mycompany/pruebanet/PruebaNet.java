7997917ba782539ebbba40e63f328c75f4ad6b25
Sara <sara.94.coslada@digitechfp.com>
2024-11-25 21:00:18 +0000

    Creación PruebaSimple1 Signed-off-by:Sara <sara.94.coslada@digitechfp.com>

---
+++ PruebaNet/src/main/java/com/mycompany/pruebanet/PruebaNet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebanet;
+
+/**
+ *
+ * @author David
+ */
+public class PruebaNet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
